5ba7c038f0c30a29d660048ec46cf6d7f9ce0752
atom-tr <thai.tr@hotmail.com>
2024-12-18 17:20:10 +0700

    remove submodule

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,3 @@
-import { SITE as ASTRO_SITE, SOCIAL_LINKS as ASTRO_SOCIAL_LINKS } from '@/shared-configs/astro';
 
 export type Site = {
   TITLE: string
@@ -15,7 +14,14 @@ export type Link = {
   icon?: string
 }
 
-export const SITE: Site = ASTRO_SITE
+export const SITE: Site = {
+  TITLE: 'atomtr',
+  DESCRIPTION: 'A developer / DevOps engineer with a passion for automating everything.',
+  EMAIL: 'thai.tr@hotmail.com',
+  NUM_POSTS_ON_HOMEPAGE: 2,
+  POSTS_PER_PAGE: 3,
+  SITEURL: 'https://atomtr.link',
+}
 
 export const NAV_LINKS: Link[] = [
   { href: '/blog', label: 'blog' },
@@ -24,4 +30,10 @@ export const NAV_LINKS: Link[] = [
   { href: '/tags', label: 'tags' },
 ]
 
-export const SOCIAL_LINKS: Link[] = ASTRO_SOCIAL_LINKS
+export const SOCIAL_LINKS: Link[] = [
+  { href: 'thai.tr@hotmail.com', label: 'Email', icon: 'lucide:mail' },
+  { href: 'https://github.com/atom-tr', label: 'Github', icon: 'lucide:github' },
+  { href: 'https://instagram.com/rt.mota', label: 'Instagram', icon: 'lucide:instagram' },
+  { href: 'https://dev.to/atomtr', label: 'Dev.to' },
+  { href: '/rss.xml', label: 'RSS', icon: 'lucide:rss' }
+]
